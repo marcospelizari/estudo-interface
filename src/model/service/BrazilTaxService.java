672d9d2d79bfcaacdb9cb178de644632c04f9c5e
Marcos <132465567+marcospelizari@users.noreply.github.com>
2025-12-19 11:51:20 -0300

    refactor: implementacao interface

--- src/model/service/BrazilTaxService.java
+++ src/model/service/BrazilTaxService.java
@@ -1,6 +1,7 @@
 package model.service;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService {
+
 
     public double tax(double amount) {
         if (amount <= 100.0) {
